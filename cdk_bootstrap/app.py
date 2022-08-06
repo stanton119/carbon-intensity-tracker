85c6db599c823207d65b0bfdaf81c95327c361df
Richard Stanton <stanton1191@gmail.com>
2022-08-06 11:40:22 +0000

    adding codepipeline cdk

--- cdk_bootstrap/app.py
+++ cdk_bootstrap/app.py
@@ -4,8 +4,10 @@ import os
 import aws_cdk as cdk
 
 from stacks.lambda_api_call import LambdaAPICallStack
+from stacks.code_pipeline import CodePipelineStack
 
 
 app = cdk.App()
-LambdaAPICallStack(app, "LambdaApiCallStack")
+CodePipelineStack(app, "CodePipelineStack")
+# LambdaAPICallStack(app, "LambdaApiCallStack")
 app.synth()
